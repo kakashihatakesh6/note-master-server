311fdbee2ddaf5ac98b966cfc4181dfcf304b60a
Nikhil Dasar <kakashihatakesh6@gmail.com>
2024-06-22 00:05:21 +0530

    main

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const cors = require('cors');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const { axios } = require('axios');
 
 const app = express();
 const port = process.env.PORT || 5000;
+const cors = require('cors');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
